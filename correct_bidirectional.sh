e0d769b518c96d225154591a5e5c35d2a3f252f8
Harish <harish0201@users.noreply.github.com>
2021-06-08 23:22:25 +0530

    parallelized to #of samples
    Checks the primers for the first file set i.e R1 and its R2 pair based on the `ls *R1.fastq` listing

--- correct_bidirectional.sh
+++ correct_bidirectional.sh
@@ -1,32 +1,33 @@
-## used for bidirectional sequencing, which was popular a few years back on 454 and Illumina platforms. 
-##put primer1 and 2 as commandline args, seqkit and bbmap installation directory path to be given.
 #!/bin/bash
+
 mkdir -p proc_fastq/paired
 owd=$PWD
 primer1="$1"
 primer2="$2"
 seqkit_bin="$3"
 bbmap_bin="$4"
-export PATH=$PATH:"$3"
-#
-for i in *_R1.fastq; do seqkit grep -d -i -p $primer1 "$i" | seqkit seq -v >> proc_fastq/"$i" ; \ 
+parallel_fac="$5"
+printf "\nparallel_fac impacts on the number of seqkit processes run; i.e number of samples processed in a single time\n"
+export PATH=$PATH:"$3" 
+printf "\nstarting the process\n"
+for i in *_R1.fastq; do echo "seqkit grep -d -i -p $primer1 "$i" | seqkit seq -v >> proc_fastq/"$i" ; \ 
 seqkit grep -d -i -p $primer2 $(basename "$i" R1.fastq)R2.fastq | seqkit seq -v >> proc_fastq/$(basename "$i" R1.fastq)R2.fastq; \
 seqkit grep -d -i -p $primer2 "$i" | seqkit seq -r -p -v >> proc_fastq/"$i"; \
-seqkit grep -d -i -p $primer1 $(basename "$i" R1.fastq)R2.fastq | seqkit seq -r -p -v >> proc_fastq/$(basename "$i" R1.fastq)R2.fastq; \
-done
+seqkit grep -d -i -p $primer1 $(basename "$i" R1.fastq)R2.fastq | seqkit seq -r -p -v >> proc_fastq/$(basename "$i" R1.fastq)R2.fastq"; \
+done | parallel -j$parallel_fac
 #
-echo "orienting done"
+printf "\norienting done\n"
 #
 cd proc_fastq
 for i in *R1.fastq; do "$4"/repair.sh in="$i" in2=$(basename "$i" R1.fastq)R2.fastq \
 out=paired/"$i" out2=paired/$(basename "$i" R1.fastq)R2.fastq ain=t; done
 #
-echo "re-pairing data"
-echo "checking if orientation is correct or not"
+printf "\nre-pairing data\n"
+printf "\nchecking if orientation is correct or not\n"
 #
 cd paired
 Rscript $owd/primer_checks.R "$1" "$2"
-echo "check proc_fastq/primer_hits.txt for final summary"
+printf "\ncheck proc_fastq/primer_hits.txt for final summary\n"
 rm -rf filtN
 rm ../*fastq
 mv *.fastq ../
@@ -36,7 +37,7 @@ rm -rf paired
 #
 cd $owd
 #
-echo "files written to proc_fastq/ primer hits written to primer_hits.txt"
-echo "other primer pairs can be checked independently with primer_checks.R. Use it as\
-Rscript primer_checks.R primer1 primer2"
-echo "done"
+printf "\nfiles written to proc_fastq/ primer hits written to primer_hits.txt\n"
+printf "\nother primer pairs can be checked independently with primer_checks.R. Use it as\n
+Rscript primer_checks.R primer1 primer2\n"
+printf "\ndone\n"
